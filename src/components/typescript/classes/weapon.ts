58b48fcff93976b7b446d6531788100e6643f4b2
Adrian Schubert <adrian.schubert@arcor.de>
2024-11-13 08:11:05 +0100

    added all weapons

--- src/components/typescript/classes/weapon.ts
+++ src/components/typescript/classes/weapon.ts
@@ -1,36 +1,36 @@
 export class Weapon {
   name: string //Name der Waffe
   imagePath: string //Pfad des Kartenbildes
-  //range: number[] //Angriffsreichtweite
+  range: number[] //Angriffsreichtweite
   dices: number //Anzahl der Würfel beim Angriff
   hit: number //Mindestwürfelzahl für einen Treffer
-  //dmg: number //Schaden bei Treffer
+  dmg: number //Schaden bei Treffer
   isMeele: boolean //True bei Meele; false bei Range
-  //canOpenDoors: boolean //Kann diese Waffe Türen öffnen?
-  //isSilentOpeningDoors: boolean //Öffnet Türen ohne Lärm zu machen?
-  //isSilentAttacking: boolean //Greift an ohne Lärm zu machen?
+  canOpenDoors: boolean //Kann diese Waffe Türen öffnen?
+  isSilentOpeningDoors: boolean //Öffnet Türen ohne Lärm zu machen?
+  isSilentAttacking: boolean //Greift an ohne Lärm zu machen?
 
   constructor(
     name: string,
     imagePath: string,
-    //range: number[],
+    range: number[],
     dices: number,
     hit: number,
-    //dmg: number,
+    dmg: number,
     isMeele: boolean,
-    //canOpenDoors: boolean,
-    //isSilentOpeningDoors: boolean,
-    //isSilentAttacking: boolean,
+    canOpenDoors: boolean,
+    isSilentOpeningDoors: boolean,
+    isSilentAttacking: boolean,
   ) {
     this.name = name
     this.imagePath = imagePath
-    //this.range = range
+    this.range = range
     this.dices = dices
     this.hit = hit
-    //this.dmg = dmg
+    this.dmg = dmg
     this.isMeele = isMeele
-    //this.canOpenDoors = canOpenDoors
-    //this.isSilentOpeningDoors = isSilentOpeningDoors
-    //this.isSilentAttacking = isSilentAttacking
+    this.canOpenDoors = canOpenDoors
+    this.isSilentOpeningDoors = isSilentOpeningDoors
+    this.isSilentAttacking = isSilentAttacking
   }
 }
