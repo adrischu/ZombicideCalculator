fd9922906ab73677523f798b66b80cd2edbe6f26
Adrian Schubert <adrian.schubert@arcor.de>
2024-11-13 23:50:49 +0100

    added multiple Weapon Choice

--- src/App.vue
+++ src/App.vue
@@ -8,33 +8,40 @@
     <v-card-text>
       <v-tabs-window v-model="tab">
         <v-tabs-window-item value="window-bonus">
-          <plus-minus v-model="extraDiceAll">Extra dice for all attacks</plus-minus>
-          <plus-minus v-model="extraDiceMeele">Extra dice for meele attacks</plus-minus>
-          <plus-minus v-model="extraDiceRange">Extra dice for ranged attacks</plus-minus>
-          <plus-minus v-model="extraResultAll">Extra result for all attacks</plus-minus>
-          <plus-minus v-model="extraResultMeele">Extra result for meele attacks</plus-minus>
-          <plus-minus v-model="extraResultRange">Extra result for ranged attacks</plus-minus>
+          <plus-minus v-model="extra.diceAll">Extra dice for all attacks</plus-minus>
+          <plus-minus v-model="extra.diceMeele">Extra dice for meele attacks</plus-minus>
+          <plus-minus v-model="extra.diceRange">Extra dice for ranged attacks</plus-minus>
+          <plus-minus v-model="extra.resultAll">Extra result for all attacks</plus-minus>
+          <plus-minus v-model="extra.resultMeele">Extra result for meele attacks</plus-minus>
+          <plus-minus v-model="extra.resultRange">Extra result for ranged attacks</plus-minus>
         </v-tabs-window-item>
         <v-tabs-window-item value="window-weapons">
-          <v-select
-            v-model="selectedWeapon"
-            :items="weapons"
-            item-title="name"
-            return-object
-            hide-details
-          >
-          </v-select>
-          <v-checkbox v-model="useDualWielding" label="Use Dual Wielding?"></v-checkbox>
-          <v-img :src="selectedWeapon?.imagePath" max-width="200" alt="Weapon Image"></v-img>
+          <div v-for="(selectedWeapon, index) in selectedWeapons" :key="selectedWeapon.name">
+            <v-select
+              v-model="selectedWeapons![index]"
+              :items="weapons"
+              item-title="name"
+              return-object
+              hide-details
+            >
+            </v-select>
+            <v-checkbox
+              v-model="selectedWeapon.useDualWielding"
+              label="Use Dual Wielding?"
+            ></v-checkbox>
+            <v-img :src="selectedWeapon.imagePath" max-width="200" alt="Weapon Image"></v-img>
+            <v-btn @click="selectedWeapons?.splice(index, 1)">-</v-btn>
+          </div>
+          <v-btn @click="addWeapon()">New</v-btn>
         </v-tabs-window-item>
         <v-tabs-window-item value="window-results">
           <plus-minus v-model="toKill">Zombies to kill</plus-minus>
           <plus-minus v-model="actions">Actions available</plus-minus>
-          <div>
-            Propability to kill at least {{ toKill }} zombies with {{ actions }} actions:
-            {{ propability }}%
+          <div v-for="selectedWeapon in selectedWeapons" :key="selectedWeapon.name">
+            {{ selectedWeapon.name }}: Propability to kill at least {{ toKill }} zombies with
+            {{ actions }} actions: {{ selectedWeapon.propability(actions, toKill, extra) }}%
+            <div>Expected kills: {{ selectedWeapon.expected(actions, extra) }}</div>
           </div>
-          <div>Expected kills: {{ expected }}</div>
         </v-tabs-window-item>
       </v-tabs-window>
     </v-card-text>
@@ -45,44 +52,25 @@
 <script setup lang="ts">
 import { weapons } from './stores/weaponStore'
 import { Weapon } from './components/typescript/classes/weapon'
-import { computed, ref } from 'vue'
-import { bernoulliMindestens } from './components/typescript/stochastics'
+import { ref, type Ref } from 'vue'
 import plusMinus from './components/plus-minus.vue'
 // <img :src="`./assets/card-images/${selectedWeapon!.imagePath}`" />
-const selectedWeapon = ref<Weapon | null>(weapons[0])
-const useDualWielding = ref(false)
+const selectedWeapons: Ref<Weapon[] | null> = ref<Weapon[] | null>([weapons[0]])
 const toKill = ref(2)
 const actions = ref(3)
 const tab = ref('window-bonus')
-const extraDiceMeele = ref(0)
-const extraDiceRange = ref(0)
-const extraDiceAll = ref(0)
-const extraResultMeele = ref(0)
-const extraResultRange = ref(0)
-const extraResultAll = ref(0)
-
-const p = computed(() => {
-  let p = (6 + 1 - selectedWeapon.value!.hit + extraResultAll.value) / 6
-  if (selectedWeapon.value!.isMeele) p += extraResultMeele.value //Falls Waffe Meele ist extra Meele-Würfel dazuzählen
-  if (!selectedWeapon.value!.isMeele) p += extraResultRange.value //Falls Waffe Ranged ist extra Ranged-Würfel dazu zählen
-  return p
+const extra = ref({
+  diceAll: 0,
+  diceMeele: 0,
+  diceRange: 0,
+  resultAll: 0,
+  resultMeele: 0,
+  resultRange: 0,
 })
 
-const dices = computed(() => {
-  let dices = selectedWeapon.value!.dices + extraDiceAll.value //Würfel von Waffe + Extra Würfel(Alle) dazu
-  if (selectedWeapon.value!.isMeele) dices += extraDiceMeele.value //Falls Waffe Meele ist extra Meele-Würfel dazuzählen
-  if (!selectedWeapon.value!.isMeele) dices += extraDiceRange.value //Falls Waffe Ranged ist extra Ranged-Würfel dazu zählen
-  dices = useDualWielding.value ? dices * 2 : dices //Wenn Waffe doppelhändig geführt wird Würfel verdoppeln
-  return dices
-})
-
-const propability = computed(() => {
-  return Math.round(100 * bernoulliMindestens(p.value, actions.value * dices.value, toKill.value))
-})
-
-const expected = computed(() => {
-  return p.value * actions.value * dices.value
-})
+function addWeapon() {
+  selectedWeapons.value!.push(weapons[0])
+}
 </script>
 
 <style scoped>
--- src/components/typescript/classes/weapon.ts
+++ src/components/typescript/classes/weapon.ts
@@ -1,3 +1,5 @@
+import { bernoulliMindestens } from '../stochastics'
+
 export class Weapon {
   name: string //Name der Waffe
   imagePath: string //Pfad des Kartenbildes
@@ -9,10 +11,11 @@ export class Weapon {
   canOpenDoors: boolean //Kann diese Waffe Türen öffnen?
   isSilentOpeningDoors: boolean //Öffnet Türen ohne Lärm zu machen?
   isSilentAttacking: boolean //Greift an ohne Lärm zu machen?
+  useDualWielding: boolean //Zeigt an ob die Waffe aktuell mit Dual Wielding benutzt wird
+  reload: boolean
 
   constructor(
     name: string,
-    imagePath: string,
     range: number[],
     dices: number,
     hit: number,
@@ -21,6 +24,8 @@ export class Weapon {
     canOpenDoors: boolean,
     isSilentOpeningDoors: boolean,
     isSilentAttacking: boolean,
+    reload: boolean,
+    imagePath: string,
   ) {
     this.name = name
     this.imagePath = imagePath
@@ -32,5 +37,67 @@ export class Weapon {
     this.canOpenDoors = canOpenDoors
     this.isSilentOpeningDoors = isSilentOpeningDoors
     this.isSilentAttacking = isSilentAttacking
+    this.useDualWielding = false
+    this.reload = reload
+  }
+
+  p(extra: {
+    diceAll: number
+    diceMeele: number
+    diceRange: number
+    resultAll: number
+    resultMeele: number
+    resultRange: number
+  }) {
+    let p = (6 + 1 - this.hit + extra.resultAll) / 6
+    if (this.isMeele) p += extra.resultMeele / 6 //Falls Waffe Meele ist extra Meele-Würfel dazuzählen
+    if (!this.isMeele) p += extra.resultRange / 6 //Falls Waffe Ranged ist extra Ranged-Würfel dazu zählen
+    return p
+  }
+
+  calcDices(extra: {
+    diceAll: number
+    diceMeele: number
+    diceRange: number
+    resultAll: number
+    resultMeele: number
+    resultRange: number
+  }) {
+    let dices = this.dices + extra.diceAll //Würfel von Waffe + Extra Würfel(Alle) dazu
+    if (this.isMeele) dices += extra.diceMeele //Falls Waffe Meele ist extra Meele-Würfel dazuzählen
+    if (!this.isMeele) dices += extra.diceRange //Falls Waffe Ranged ist extra Ranged-Würfel dazu zählen
+    dices = this.useDualWielding ? dices * 2 : dices //Wenn Waffe doppelhändig geführt wird Würfel verdoppeln
+    return dices
+  }
+
+  propability(
+    actions: number,
+    toKill: number,
+    extra: {
+      diceAll: number
+      diceMeele: number
+      diceRange: number
+      resultAll: number
+      resultMeele: number
+      resultRange: number
+    },
+  ): number {
+    return Math.round(
+      100 * bernoulliMindestens(this.p(extra), actions * this.calcDices(extra), toKill),
+    )
+  }
+
+  expected(
+    actions: number,
+    extra: {
+      diceAll: number
+      diceMeele: number
+      diceRange: number
+      resultAll: number
+      resultMeele: number
+      resultRange: number
+    },
+  ) {
+    return this.p(extra) * actions * this.calcDices(extra)
   }
 }
